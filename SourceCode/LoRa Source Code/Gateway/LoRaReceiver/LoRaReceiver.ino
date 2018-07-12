56597ff03f8861c0801f07dd87766da873c27234
nguyenmanhthao996tn <manhthao.spm@gmail.com>
2018-07-12 16:59:56 +0700

    Update: Gateway LoRa code, can't work with interrupt because of hardware issue

--- SourceCode/LoRa Source Code/Gateway/LoRaReceiver/LoRaReceiver.ino
+++ SourceCode/LoRa Source Code/Gateway/LoRaReceiver/LoRaReceiver.ino
@@ -3,44 +3,38 @@
 
 void setup()
 {
-    Serial.begin(115200);
-    while (!Serial)
-        ;
+  Serial.begin(115200);
+  Serial.println("LoRa Receiver");
 
-    Serial.println("LoRa Receiver");
+  LoRa.setPins(15, 16, 5);
+  if (!LoRa.begin(868000000))
+  {
+    Serial.println("Starting LoRa failed!");
+    while (1)
+      ;
+  }
 
-    if (!LoRa.begin(868000000))
-    {
-        Serial.println("Starting LoRa failed!");
-        while (1)
-            ;
-    }
-
-    LoRa.setPins(15, 16, 5);
-
-    LoRa.setSignalBandwidth(500000);
-    LoRa.setCodingRate4(5);
-    LoRa.setSpreadingFactor(12);
-    LoRa.setPreambleLength(8);
-    LoRa.setSyncWord(0x24);
+  LoRa.setSignalBandwidth(500000);
+  LoRa.setCodingRate4(5);
+  LoRa.setSpreadingFactor(12);
+  LoRa.setPreambleLength(8);
+  LoRa.setSyncWord(0x24);
 }
 
 void loop()
 {
-    // try to parse packet
     int packetSize = LoRa.parsePacket();
     if (packetSize)
     {
-        // received a packet
-        Serial.print("Received packet '");
+      Serial.print(packetSize);
+      Serial.print(" Received packet '");
+  
+      while (LoRa.available())
+      {
+        Serial.print((char)LoRa.read());
+      }
 
-        // read packet
-        while (LoRa.available())
-        {
-            Serial.print((char)LoRa.read());
-        }
-        // print RSSI of packet
-        Serial.print("' with RSSI ");
-        Serial.println(LoRa.packetRssi());
+      Serial.print("' with RSSI ");
+      Serial.println(LoRa.packetRssi());
     }
 }
