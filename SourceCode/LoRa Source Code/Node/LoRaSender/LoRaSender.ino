3e4ae2baedf07850ea86de97159bb89b1d911f0d
Thảo Nguyễn Mạnh <manhthao.spm@gmail.com>
2018-07-28 16:09:03 +0700

    Update: Node will send data to Gateway every 5 minutes (No ACK from Gateway yet) (Not tested)

--- SourceCode/LoRa Source Code/Node/LoRaSender/LoRaSender.ino
+++ SourceCode/LoRa Source Code/Node/LoRaSender/LoRaSender.ino
@@ -1,9 +1,16 @@
 #include <SPI.h>
 #include <LoRa.h>
+#include "OutputData.h"
 
+char sendingDataStringBuffer[255];
 String inputString = "";
 boolean stringComplete = false;
-unsigned long now, lastCounter;
+unsigned long now, lastSenderCounter, lastGetDataCounter;
+
+const uint16_t windDirectionValueArray[] = {0, 45, 90, 135, 180, 225, 270, 315};
+uint16_t windDirectionCounterArray[] = {0, 0, 0, 0, 0, 0, 0, 0};
+stcOutputData_t *averageDataObject, *newDataObject;
+uint16_t objectCounter;
 
 void serialEvent()
 {
@@ -18,10 +25,62 @@ void serialEvent()
   }
 }
 
+uint16_t addToAverage(uint16_t averageValue, uint16_t counter, uint16_t newValue)
+{
+  uint32_t tempValue = averageValue * counter + newValue;
+  return (averageValue / (counter + 1));
+}
+
+uint16_t getMostAppearValue(const uint16_t valueArray[], uint16_t counterArray[], uint16_t size)
+{
+  uint16_t maxCounter = counterArray[0];
+  uint16_t i;
+  for (i = 1; i < size; i++)
+  {
+    if (maxCounter < counterArray[i])
+    {
+      maxCounter = counterArray[i];
+    }
+  }
+
+  return valueArray[maxCounter];
+}
+
+void addNewObjectToAverageObject(stcOutputData_t *averageDataObject, stcOutputData_t *newDataObject)
+{
+  // airSpeed1Min
+  averageDataObject->airSpeed1Min = addToAverage(averageDataObject->airSpeed1Min, objectCounter, (newDataObject->airSpeed1Min));
+
+  // airSpeed5Min
+  averageDataObject->airSpeed5Min = addToAverage(averageDataObject->airSpeed5Min, objectCounter, (newDataObject->airSpeed5Min));
+
+  // temperature
+  averageDataObject->temperature = addToAverage(averageDataObject->temperature, objectCounter, (newDataObject->temperature));
+
+  // rainfall1Hour
+  averageDataObject->rainfall1Hour = addToAverage(averageDataObject->rainfall1Hour, objectCounter, (newDataObject->rainfall1Hour));
+
+  // rainfall24Hour
+  averageDataObject->rainfall24Hour = addToAverage(averageDataObject->rainfall24Hour, objectCounter, (newDataObject->rainfall24Hour));
+
+  // humidity
+  averageDataObject->humidity = addToAverage(averageDataObject->humidity, objectCounter, (newDataObject->humidity));
+
+  // atmosphere
+  averageDataObject->atmosphere = addToAverage(averageDataObject->atmosphere, objectCounter, (newDataObject->atmosphere));
+
+  objectCounter++;
+}
+
 void setup()
 {
   pinMode(4, INPUT);
   pinMode(5, INPUT);
+
+  objectCounter = 0;
+  lastGetDataCounter = 0;
+  lastSenderCounter = 0;
+  stcOutputData_Create(&averageDataObject);
 
   inputString.reserve(128); // allocate 128 bytes buffer in memory for manipulating Strings
 
@@ -49,16 +108,74 @@ void loop()
 
   if (stringComplete)
   {
-    if ((inputString.charAt(33) == '\r') && (inputString.charAt(34) == '\n') && (now - lastCounter > 5000))
+    // Check counter to get data every 5 seconds
+    if ((inputString.charAt(33) == '\r') && (inputString.charAt(34) == '\n') && (now - lastGetDataCounter > 5000))
     { // Check if string is correct
-      lastCounter = now;
+      lastGetDataCounter = now;
       inputString.setCharAt(35, '\0');
 
+      // Create and parse new object
+      stcOutputData_Parse(inputString.c_str(), &newDataObject);
+
+      // Add values from new object to average object
+      addNewObjectToAverageObject(averageDataObject, newDataObject);
+      switch (newDataObject->airDirection)
+      {
+      case 0:
+        windDirectionCounterArray[0]++;
+        break;
+      case 45:
+        windDirectionCounterArray[1]++;
+        break;
+      case 90:
+        windDirectionCounterArray[2]++;
+        break;
+      case 135:
+        windDirectionCounterArray[3]++;
+        break;
+      case 180:
+        windDirectionCounterArray[4]++;
+        break;
+      case 225:
+        windDirectionCounterArray[5]++;
+        break;
+      case 270:
+        windDirectionCounterArray[6]++;
+        break;
+      case 315:
+        windDirectionCounterArray[7]++;
+        break;
+      default:
+        break;
+      }
+
+      // Delete new object
+      stcOutputData_Delete(newDataObject);
+
+      Serial.print("Get: ");
+      Serial.println(inputString.substring(0, 35));
+    }
+
+    // Check counter to send on LoRa every 5 mins
+    if (now - lastSenderCounter > 300000)
+    {
+      lastSenderCounter = now;
+
+      // Get air direction value and set to average object
+      averageDataObject->airDirection = getMostAppearValue(windDirectionValueArray, windDirectionCounterArray, 8);
+
+      stcOutputData_ToDataString(averageDataObject, sendingDataStringBuffer);
+
       LoRa.beginPacket();
-      LoRa.print(inputString.substring(0, 35));
+      LoRa.print(sendingDataStringBuffer);
       LoRa.endPacket();
 
-      Serial.println(inputString.substring(0, 35));
+      // Reset all object buffer
+      memset((void *)windDirectionCounterArray, 0, sizeof(windDirectionCounterArray)); // windDirectionCounterArray
+      objectCounter = 0;                                                               // objectCounter
+      stcOutputData_Create(&averageDataObject);                                        // averageDataObject
+
+      Serial.println("Sent");
     }
 
     inputString = "";
---
+++ SourceCode/LoRa Source Code/Node/LoRaSender/OutputData.h
@@ -0,0 +1,70 @@
+/**
+   @file OutputData.h
+   @brief Library for managing data output string received from STC15
+   @author Thao Nguyen Manh
+   @modified June 21th 2018, 09:57
+*/
+
+#ifndef __OUTPUT_DATA_H__
+#define __OUTPUT_DATA_H__
+
+/**************** INCLUDE ****************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+/**************** DATA STRUCTURE ****************/
+typedef struct
+{
+  char *dataString;
+  uint16_t airDirection;
+  uint16_t airSpeed1Min;
+  uint16_t airSpeed5Min;
+  uint16_t temperature;
+  uint16_t rainfall1Hour;
+  uint16_t rainfall24Hour;
+  uint8_t humidity;
+  uint32_t atmosphere;
+} stcOutputData_t;
+
+/**************** METHODS ****************/
+/**
+   @brief Create a new struct of stcOutputData_t
+   @param stcOutputDataStructure Pointer to stcOutputData_t to be create
+   @return Return 1 on creating successfully
+*/
+int stcOutputData_Create(stcOutputData_t **stcOutputDataStructure);
+
+/**
+   @brief Get data from input string and put it in stcOutputData_t struct that stcOutputDataStructure point to
+   @param inputString Input data string to get data
+   @param stcOutputDataStructure Pointer point to struct that storage data
+   @return Return 1 on parsing successfully
+*/
+int stcOutputData_Parse(char *inputString, stcOutputData_t **stcOutputDataStructure);
+
+/**
+   @brief Convert a struct of stcOutputData_t to string for printing on console
+   @param stcOutputDataStructure Structure to convert
+   @param buffer Character array to print
+   @return Void
+*/
+void stcOutputData_ToString(stcOutputData_t *stcOutputDataStructure, char *buffer);
+
+/**
+   @brief Convert a struct of stcOutputData_t to data string same as input string from value of object
+   @param stcOutputDataStructure Structure to convert
+   @param buffer Character array to print
+   @return Void
+*/
+void stcOutputData_ToDataString(stcOutputData_t *stcOutputDataStructure, char *buffer);
+
+/**
+   @brief Free and delete stcOutputDataStructure pointer
+   @param Pointer to be delete
+   @return Void
+*/
+void stcOutputData_Delete(stcOutputData_t *stcOutputDataStructure);
+
+#endif /* __OUTPUT_DATA_H__ */
