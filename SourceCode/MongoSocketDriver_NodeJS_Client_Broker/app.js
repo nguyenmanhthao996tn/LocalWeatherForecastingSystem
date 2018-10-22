2b5770df0c2b7071817ccbdfad97ba81567c8fa3
Thảo Nguyễn Mạnh <manhthao.spm@gmail.com>
2018-10-22 15:39:49 +0700

    Update: Fix small bugs in Broker

--- SourceCode/MongoSocketDriver_NodeJS_Client_Broker/app.js
+++ SourceCode/MongoSocketDriver_NodeJS_Client_Broker/app.js
@@ -25,10 +25,17 @@ server.on('clientConnected', function (client) {
 
 // fired when a message is received
 server.on('published', function (packet, client) {
-  var str = packet.payload.toString();
-  consoleLogWithISODate('Publish ' + packet.payload + ' | Length: ' + str.length);
+  var inputString = packet.payload.toString();
+  consoleLogWithISODate('Publish ' + inputString + ' | Length: ' + inputString.length);
 
-  if ((str.length == 17) && (str[15] == '\r') && (str[16] == '\n')) {
+  var inputSplitedString = inputString.split(',');
+  var stackedValue = [];
+  for (var i = 0; i < inputSplitedString.length; i++) {
+    var temp = parseInt(inputSplitedString[i]);
+    stackedValue.push(temp);
+  }
+
+  if ((stackedValue.length == 17) && (stackedValue[15] == 13) && (stackedValue[16] == 10)) {
     var client = new net.Socket();
 
     client.on('data', function (data) {
@@ -43,7 +50,7 @@ server.on('published', function (packet, client) {
     client.connect(5001, MONGO_SERVER_NAME, function () {
       consoleLogWithISODate('Connected');
 
-      var weatherDataWriteRequestObject = getWriteRequestObject(packet.payload.toString());
+      var weatherDataWriteRequestObject = getWriteRequestObject(stackedValue);
       // weatherDataWriteRequestObject.nodeId = packet.payload.toString().substring(0, 15);
       // weatherDataWriteRequestObject.data = packet.payload.toString().substring(15);
       client.write(JSON.stringify(weatherDataWriteRequestObject));
@@ -65,18 +72,18 @@ function consoleLogWithISODate(str) {
   console.log(n + ': ' + str);
 }
 
-function getWriteRequestObject(payloadString) {
+function getWriteRequestObject(stackedValue) {
   var weatherDataWriteRequestObject = { code: 1, nodeId: '', data: '' };
-  var nodeIdHighByte = payloadString.charCodeAt(0);
-  var nodeIdLowByte = payloadString.charCodeAt(1);
-  
+  var nodeIdHighByte = stackedValue[0];
+  var nodeIdLowByte = stackedValue[1];
+
   weatherDataWriteRequestObject.nodeId = ((nodeIdHighByte << 8) | nodeIdLowByte).toString();
 
   var buffer = "";
-  for (var i = 0; i < payloadString.length - 3; i++) {
-    buffer += payloadString.charCodeAt(i).toString() + ",";
+  for (var i = 0; i < stackedValue.length - 3; i++) {
+    buffer += stackedValue[i].toString() + ",";
   }
-  buffer += payloadString.charCodeAt(payloadString.length - 3);
+  buffer += stackedValue[stackedValue.length - 3];
   weatherDataWriteRequestObject.data = buffer;
 
   return weatherDataWriteRequestObject;
